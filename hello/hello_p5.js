d87113852d48ecda901c4b6a7ea4ebaa2e183022
hileela <leelamandal23@marlborough.org>
2021-09-08 14:34:41 -0700

    fixed leelas mistakes

--- hello/hello_p5.js
+++ hello/hello_p5.js
@@ -1,6 +1,8 @@
 function setup() {
   createCanvas(400, 400);
   background(0);
+
+
   for(let i=1; i<10, i++){
     fill(random(256),random(256),random(256));
     let x = random(0, width);
@@ -15,13 +17,7 @@ let x = 50;
 
 
 function draw() {
-  background(0);
-  fill(255,0,0);
-  ellipse(x, 200, 100, 50);
-  x++;
 
-  if (x > width+50)
-    x = -50;
 }
 
 function mousePressed(){
